9dd3f10660c458e901292bc1cc530bf18a347e12
Harry Oliver <someonecancode@yahoo.co.uk>
2016-01-12 15:27:40 +0000

    fix routes typo

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get 'posts/show'
 
   resources :posts
-  root 'post#index'
+  root 'posts#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
